2578fa828d89a454ea3db5def32afe9d3d8ad4c9
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-07-17 16:18:21 +0530

    added function to borrow books

--- controllers/Book.go
+++ controllers/Book.go
@@ -63,3 +63,99 @@ func CreateBookPostForm(c *gin.Context){
 	})
 }
 
+func BorrowBook(c *gin.Context){
+
+	var book models.Book
+
+	book_id := c.Param("book_id")
+
+	result := config.DB.Where("book_id = ?", book_id).First(&book)
+	if result.Error != nil {
+     c.JSON(200, gin.H{
+		"message": "Invalid Book ID",
+	 })
+	 return
+	}
+
+	//Check if the book is already borrowed
+	if book.Borrowed == true {
+		c.JSON(200, gin.H{
+			"message": "Book is already borrowed",
+		})	
+		return
+	}
+
+	config.DB.Model(&book).Update("borrowed", true)
+
+	var borrowed models.Borrowed
+
+	borrowed.BorrowedID = uuid.New()
+
+	time_needed_str := c.PostForm("borrow_period") // this is in days
+
+	if time_needed_str == "" {
+		time_needed_str = "7" // if no time is given then default is 7 days
+	}
+
+	time_needed, _ := strconv.Atoi(time_needed_str)
+
+	borrowed.BookID = book.BookID
+	borrowed.StartTime = time.Now()
+	borrowed.EndTime = time.Now().AddDate(0, 0, time_needed)
+
+	config.DB.Create(&borrowed)
+
+	c.JSON(200, gin.H{
+		"status": "Book Borrowed Successfully",
+		"borrowed_id": borrowed.BorrowedID,
+		"book_id": borrowed.BookID,
+		"start_time": borrowed.StartTime,
+		"end_time": borrowed.EndTime,
+		"returned": borrowed.Returned,
+	})
+}
+
+
+func ReturnBook(c *gin.Context){
+
+	var borrowed models.Borrowed
+	var book models.Book
+
+	book_id := c.Param("book_id")
+	borrow_id := c.Param("borrow_id")
+
+	result := config.DB.Where("book_id = ?", book_id).First(&book)
+	if result.Error != nil {
+     c.JSON(200, gin.H{
+		"message": "Invalid Book ID",
+	 })
+	 return
+	}
+
+	result = config.DB.Where("borrowed_id = ?", borrow_id).First(&borrowed)
+	if result.Error != nil {
+		c.JSON(200, gin.H{
+			"message": "Invalid Borrow ID",
+		})	
+		return
+	}
+
+	if borrowed.Returned == true {
+		c.JSON(200, gin.H{
+			"message": "Book is already returned",
+		})	
+		return
+	}
+
+	borrowed.ReturnedAt = time.Now()
+
+	config.DB.Model(&book).Update("borrowed", false)
+	config.DB.Model(&borrowed).Update("returned", true)
+
+	c.JSON(200, gin.H{
+		"status": "Book Returned Successfully",
+		"borrowed_id": borrowed.BorrowedID,
+		"book_id": borrowed.BookID,
+	})
+
+}
